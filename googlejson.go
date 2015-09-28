cc5f16e86eda71259f965a291e2e5cdec0bb0aff
Jason <jasonrichardsmith@gmail.com>
2015-09-28 15:59:37 -0400

    added docs

--- googlejson.go
+++ googlejson.go
@@ -15,25 +15,35 @@ import (
 	"strings"
 )
 
-/*
-
-
- */
+// Type is the top level json object.
+// It should contain at least one data object or one error.
 type Response struct {
-	APIVersion string            `json:"apiVersion"`
-	Context    string            `json:"context"`
-	ID         string            `json:"id"`
-	Method     string            `json:"method"`
-	Params     map[string]string `json:"params"`
-	Data       `json:"datai, omitempty"`
-	Error      `json:"error, omitempty"`
+	// Version of API being served or received.
+	APIVersion string `json:"apiVersion"`
+	// Context is a parameter submitted by requestor
+	// as in an http.Request, this will be returned
+	// to the client for context.
+	Context string `json:"context"`
+	// ID is a unique ID assigned to the request
+	// if the API will need to reference a transaction.
+	ID string `json:"id"`
+	// Method represents the operation performed.
+	Method string `json:"method"`
+	// Params are a list of parameters submitted to the API.
+	Params map[string]string `json:"params"`
+	// Data holds the actual data that was returned.
+	Data `json:"datai, omitempty"`
+	// Errors to be returned.
+	Error `json:"error, omitempty"`
 }
 
+// Shortcut to create a new Response
 func New() *Response {
 	r := Response{Params: make(map[string]string), Data: NewData()}
 	return &r
 }
 
+// Shortcut to create a response from an http.Response
 func NewFromResponse(r http.Response) (*Response, err) {
 	res := New()
 	defer r.Body.Close()
@@ -45,6 +55,8 @@ func NewFromResponse(r http.Response) (*Response, err) {
 	return res, err
 }
 
+// Shortcut to create a copy of the response.  This
+// means you can set common settings, and resuse the base Response.
 func (r *Response) Copy() *Response {
 	nr := Response{
 		APIVersion: r.APIVersion,
@@ -54,10 +66,12 @@ func (r *Response) Copy() *Response {
 	return &nr
 }
 
+// Write the struct to byte[]
 func (r *Response) Write() ([]byte, error) {
 	return json.Marshal(r)
 }
 
+// Shortcut to write to an http.ResponseWriter.
 func (r *Response) WriteToResponse(w http.ResponseWriter) error {
 	b, err := r.Write()
 	if err != nil {
@@ -67,28 +81,53 @@ func (r *Response) WriteToResponse(w http.ResponseWriter) error {
 	return nil
 }
 
+// Data structure holds all information specific to the data in the
+// Response and the dat6a itself
 type Data struct {
-	Title            string            `json:"title"`
-	Description      string            `json:"description"`
-	Kind             string            `json:"kind"`
-	Fields           string            `json:"fields"`
-	Etag             string            `json:"etag"`
-	ID               string            `json:"id"`
-	Lang             string            `json:"lang"`
-	Updated          string            `json:"updated"`
-	Deleted          bool              `json:"deleted"`
-	CurrentItemCount int               `json:"currentItemCount"`
-	ItemsPerPage     int               `json:"itemsPerPage"`
-	StartIndex       int               `json:"startIndex"`
-	TotalItems       int               `json:"totalItems"`
-	PageIndex        int               `json:"pageIndex"`
-	TotalPages       int               `json:"totalPages"`
-	NextLink         string            `json:"nextLink"`
-	PreviousLink     string            `json:"previousLink"`
-	SelfLink         string            `json:"selfLink"`
-	EditLink         string            `json:"editLink"`
-	Items            []json.RawMessage `json:"items"`
-	item             int
+	// Kind is a name of the entity being returned, such as
+	// cars, orders, customers etc.
+	Kind string `json:"kind"`
+	// A list of field being returned.  This is a comma separated
+	// string so helper methods are provided below.
+	Fields string `json:"fields"`
+	// Etags are an ID for the version of the data you are viewing
+	// this allows to identify expired data.
+	// More can be read here:
+	// https://developers.google.com/gdata/docs/2.0/reference?csw=1#ResourceVersioning
+	Etag string `json:"etag"`
+	// The ID for this request
+	ID string `json:"id"`
+	// Language.
+	Lang string `json:"lang"`
+	// Last time record was updated
+	Updated string `json:"updated"`
+	// Deleted, if this was a delete request did it occur.
+	Deleted bool `json:"deleted"`
+	// How many items have returned this request.
+	CurrentItemCount int `json:"currentItemCount"`
+	// How many items could be returned in each request.
+	ItemsPerPage int `json:"itemsPerPage"`
+	// Where the list begins in the full list of return values.
+	StartIndex int `json:"startIndex"`
+	// Total number of Items matching request.
+	TotalItems int `json:"totalItems"`
+	// Current page.
+	PageIndex int `json:"pageIndex"`
+	// Total number of pages.
+	TotalPages int `json:"totalPages"`
+	// Link to next result or result set for paginated results.
+	NextLink string `json:"nextLink"`
+	// Link to previous result or result set for paginated results.
+	PreviousLink string `json:"previousLink"`
+	// Direct link to current data set.
+	SelfLink string `json:"selfLink"`
+	// Link to edit results.
+	EditLink string `json:"editLink"`
+	// An array of items -  this is the actual data.
+	Items []json.RawMessage `json:"items"`
+
+	// pointer to current item.
+	item int
 }
 
 func NewData() *Data {
